189a8c39cdd07210fc3700302000930808e7cba2
BareninVitalya <36921298+BareninVitalya@users.noreply.github.com>
2018-03-10 14:56:35 +0300

    lesson 4

---
+++ Barenin_Vitaly_Task04/Task1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class User
+    {
+        private string secondName;
+        private string name;
+        private string patronymic;
+        private string dateOfBirth;
+        private int age;
+        Regex regex = new Regex(@"\b[A-ZА-Я][a-zа-я][^0-9\s]\S+\b");
+        public string SecondName
+        {
+            get { return secondName; }
+            set { if (regex.IsMatch(value)) { secondName = value; } }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { if (regex.IsMatch(value)){ name = value; } }
+        }
+        public string Patronymic
+        {
+            get { return patronymic; }
+            set { if (regex.IsMatch(value)) { patronymic = value; } }
+        }
+        public string  DateOfBirth
+        {
+            get { return dateOfBirth; }
+            set { if (DateTime.TryParse(value,out DateTime dateTime) && (DateTime.Today.Year-Age)==dateTime.Year) { dateOfBirth = value; } }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { if (value>7 && value<100) { age = value; } }
+        }
+        public void GetInformation(User user)
+        {
+            Console.WriteLine(new string('-', 33));
+            Console.WriteLine("|{0,-20}|{1,-10}|","Name", user.Name);
+            Console.WriteLine(new string('-', 33));
+            Console.WriteLine("|{0,-20}|{1,-10}|", "Second Name", user.SecondName);
+            Console.WriteLine(new string('-', 33));
+            Console.WriteLine("|{0,-20}|{1,-10}|", "Date of birth", user.DateOfBirth);
+            Console.WriteLine(new string('-', 33));
+            Console.WriteLine("|{0,-20}|{1,-10}|", "Age", user.Age);
+            Console.WriteLine(new string('-', 33));
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+            Console.WriteLine("Enter Name: ");
+            user.Name = Console.ReadLine();
+            Console.WriteLine("Enter Age: ");
+            user.Age = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Date Of Birth: ");
+            user.DateOfBirth = Console.ReadLine();
+            Console.WriteLine("Name:{0} Age: {1} DateOfBirth: {2}", user.Name,user.Age,user.DateOfBirth);
+            user.GetInformation(user);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Barenin_Vitaly_Task04/Task3/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Triangle
+    {
+        private double a;
+        private double b;
+        private double c;
+        public double A
+        {
+            get { return a; }
+            set { if(value>0)a = value; }
+        }
+        public double B
+        {
+            get { return b; }
+            set { if (value > 0) b = value; }
+        }
+        public double C
+        {
+            get { return c; }
+            set { if (value > 0) { c = value; } }
+        }
+        public double Square(Triangle triangle)
+        {
+            double p = triangle.Perimetr(triangle)/2;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+        public Triangle(double a, double b, double c)
+        {
+            if ((b + c > a) && (a + c > b) && (a + b > c))
+            {
+                A = a;
+                B = b;
+                C = c;
+            }
+        }
+        public double Perimetr(Triangle triangle)
+        {
+            return triangle.a+triangle.b+triangle.c;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle triangle = new Triangle(2, 2, 2);
+            Console.WriteLine("Perimetr: {0}", triangle.Perimetr(triangle));
+            Console.WriteLine("Square: {0}", triangle.Square(triangle));
+            Console.ReadLine();
+        }
+    }
+}
